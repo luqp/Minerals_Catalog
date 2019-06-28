26126c3561511a1b5fc7faec755ace4252f48b61
windyludev <windyludev@outlook.com>
2019-06-28 16:42:49 -0400

    Adds the path to the debug-toolbar

--- catalog/minerals/urls.py
+++ catalog/minerals/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'minerals'
 urlpatterns = [
     path('detail/<int:pk>/', views.mineral_detail, name='detail'),
+    path('search/<str:term>/', views.search, name='search'),
+    path('search_all/<str:term>/', views.search_in_all, name='search_all'),
     path('', views.index, name='index')
 ]
