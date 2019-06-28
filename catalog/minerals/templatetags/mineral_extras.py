bb0af4008ecea4dc85b8fb5d2a8adfd9c2798c37
windyludev <windyludev@outlook.com>
2019-06-28 16:40:04 -0400

    Add two filters to that return lists for search

--- catalog/minerals/templatetags/mineral_extras.py
+++ catalog/minerals/templatetags/mineral_extras.py
@@ -28,4 +28,43 @@ def random_number():
         size = Mineral.objects.all().count()
     except:
         size = 0
-    return random.randint(1, size + 1) if size else 0
+    return random.randint(1, size) if size else 0
+
+
+@register.simple_tag
+def minerals_groups(param):
+    q_groups = Mineral.objects.values(param).distinct()
+    groups = [key[param] for key in list(q_groups)]
+    groups.sort()
+    return groups
+
+@register.simple_tag
+def minerals_colors():
+    colors_names = {
+        'beige':                '#F5F5DC',
+        'black':                '#000000',
+        'blue':                 '#0000FF',
+        'brown':                '#A52A2A',
+        'cyan':                 '#00FFFF',
+        'gold':                 '#FFD700',
+        'gray':                 '#808080',
+        'green':                '#008000',
+        'indigo':               '#4B0082',
+        'ivory':                '#FFFFF0',
+        'lavender':             '#E6E6FA',
+        'magenta':              '#FF00FF',
+        'navy':                 '#000080',
+        'olive':                '#808000',
+        'orange':               '#FFA500',
+        'pink':                 '#FFC0CB',
+        'purple':               '#800080',
+        'red':                  '#FF0000',
+        'salmon':               '#FA8072',
+        'silver':               '#C0C0C0',
+        'snow':                 '#FFFAFA',
+        'violet':               '#EE82EE',
+        'white':                '#FFFFFF',
+        'yellow':               '#FFFF00',
+    }
+    return colors_names.keys()
+
